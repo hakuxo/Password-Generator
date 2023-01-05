dda00020e64a55785027694543e21afd7a29e684
haku <jmhallmark123@gmail.com>
2023-01-05 00:41:22 -0500

    Added basic javascript and edited some notes

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,33 @@
 // Assignment code here
-var generation = document.querySelector("#generate")
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbols = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "="];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var pikachu = [];
+var numberOfCharacters;
+var password = getPasswordInfo();
 
+var generateBtn = document.querySelector("#generate");
+
+
+// When button is pressed it shows in the console 
 function generatePassword() {
   console.log("Hello, the button has been pressed");
-// Prompting user for password
+  getUserInput();
+  var password;
 
-// Validating input
+ function getUserInput() {
 
-// Generates password
+numberOfCharacters = window.prompt("Select the number of characters for your password.");
+// function generatePassword() 
+//   for (i = 0; < numbers; i++) {
+//     var index = Math.floor.parseInt(math.random() * numbers.length);
 
-// Display password in box
-// Generates in placeholder
-  return "Generated password will go here"
-}
-
+// return String.numberOfCharacters(Math.floor(Math.random() * 35) + 55);
+//   }
+//  }
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+ 
 
 // Write password to the #password input
 function writePassword() {
@@ -24,8 +36,10 @@ function writePassword() {
 
   passwordText.value = password;
 
+} 
 }
-
+console.log(password)
+var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+generateBtn.addEventListener("click", writePassword)
+}
